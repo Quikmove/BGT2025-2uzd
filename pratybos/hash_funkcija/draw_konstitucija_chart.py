9046212620b5522674282b5e9800e063a8f47f12
Kristupas Arifovas <50778281+Quikmove@users.noreply.github.com>
2025-10-06 13:55:57 +0300

    feat: enhance chart visualization with additional labels and title

--- pratybos/hash_funkcija/draw_konstitucija_chart.py
+++ pratybos/hash_funkcija/draw_konstitucija_chart.py
@@ -1,16 +1,17 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import pathlib as pl
+import math
 data_path = pl.Path("results/konstitucija.txt")
 output_path = pl.Path("results/konstitucija.png")
 if not data_path.exists():
     print("no file found")
-
     exit(1)
 df = pd.read_csv(data_path, sep=" ", header=None, names=["Lines", "Time"])
-
-plt.plot(df['Lines'], df['Time'], marker="o", linestyle="-")
-plt.xlabel("Line count")
-plt.ylabel("Time (s)")
+plt.plot(df['Lines'], df['Time'], marker="o", linestyle="-", label="konstitucija.txt")
+plt.xlabel("Eilučių skaičius")
+plt.ylabel("Laikas")
+plt.title("Konstitucija.txt eilučių skaičiaus hešavimo laikas (5 avg.)")
+plt.legend()
 plt.grid(True)
 plt.savefig(output_path)
