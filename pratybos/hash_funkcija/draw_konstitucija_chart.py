bfd5b9a9891848a95f8716c15e22b9e547bd19c5
Kristupas Arifovas <50778281+Quikmove@users.noreply.github.com>
2025-10-10 12:04:18 +0300

    feat: refactor draw_konstitucija_chart.py for improved data handling and plotting

--- pratybos/hash_funkcija/draw_konstitucija_chart.py
+++ pratybos/hash_funkcija/draw_konstitucija_chart.py
@@ -6,11 +6,13 @@ output_path = pl.Path("results/konstitucija.png")
 if not data_path.exists():
     print("no file found")
     exit(1)
-df = pd.read_csv(data_path, sep=" ", header=None, names=["Lines", "Time"])
-plt.plot(df['Lines'], df['Time'], marker="o", linestyle="-", label="konstitucija.txt")
+df = pd.read_csv(data_path, sep="\s+", engine="python")
+line_counts = df.iloc[:, 0]
+for column in df.columns[1:]:
+    plt.plot(line_counts, df[column], marker="o", linestyle="-", label=column)
 plt.xlabel("Eilučių skaičius")
 plt.ylabel("Laikas (s)")
-plt.title("Konstitucija.txt eilučių skaičiaus hešavimo laikas (5 avg.)")
-plt.legend()
+plt.title("Konstitucija.txt hešavimo laikas (5 vidurkiai)")
+plt.legend(title="Algoritmas")
 plt.grid(True)
 plt.savefig(output_path)
